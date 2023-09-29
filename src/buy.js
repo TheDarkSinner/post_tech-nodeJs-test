cd7dfa169b185deef584b94771eaba840f8f56c7
Sinners <gs.math.mm@gmail.com>
2023-09-29 17:20:29 -0300

    sell file near to test phase

--- src/buy.js
+++ src/buy.js
@@ -1,64 +1,108 @@
-const axios = require('axios')
-const ethers = require('ethers')
-require('dotenv').config()
+const axios = require("axios");
+const ethers = require("ethers");
+require("dotenv").config();
 
-const provider = new ethers.WebSocketProvider(process.env.provider)
-const wallet = new ethers.Wallet(process.env.privatekey,provider)
+const start = async () => {
+  const provider = new ethers.WebSocketProvider(process.env.provider);
+  const wallet = new ethers.Wallet(process.env.privatekey, provider);
 
-const fetch = async function () {
-    const dataList= []
-    while(dataList.length == 0){
-        const url = "https://api.post.tech/wallet-post/wallet/get-recent-action"
-        const resp = await axios.get(url)
-        const result = resp.data.data
+  const fetch = async function () {
+    const dataList = [];
+    while (dataList.length == 0) {
+      const url = "https://api.post.tech/wallet-post/wallet/get-recent-action";
+      const resp = await axios.get(url);
+      const result = resp.data.data;
 
-        
-        for (const x of result){
-            if (result.indexOf(x) < 3){
-                const {value} = x
-                const {txHash} = x
-                if (value == 0){
-                    dataList.push(txHash)
-                }
-            }
+      for (const x of result) {
+        if (result.indexOf(x) < 5) {
+          const { value } = x;
+          const { txHash } = x;
+          if (value == 0) {
+            dataList.push(txHash);
+          }
         }
+      }
     }
-    return queryShares(dataList)
+    return queryShares(dataList);
+  };
 
-}
-
-
-const queryShares = async function(list) {
-    const sharesList= []
-    for(const y of list){
-        const txInfo = await provider.getTransaction(y)
-        const decode = new ethers.Interface(["function buyShares(address, uint256 )"])
-        const data = decode.decodeFunctionData('buyShares', txInfo.data)
-        const params = [data[0], 1]
-        sharesList.push(params)
-    }
-    return placeBid(sharesList)
-}
-
-const placeBid = async function (params) {
+  const queryShares = async function (list) {
     try {
-        const contract = new ethers.Contract('0x87da6930626Fe0c7dB8bc15587ec0e410937e5DC',["function buyShares(address, uint256 )"],wallet)
-
-        for (const z of params) {
-
-            const tx = await contract.buyShares(z[0],z[1],{
-                gasLimit: 300000,
-                value: 71875000000000
-            })
-            const txhash = await tx.wait()
-            console.log(txhash.hash)
-        }
-        return start()
+      const sharesList = [];
+      for (const y of list) {
+        const txInfo = await provider.getTransaction(y);
+        const decode = new ethers.Interface([
+          "function buyShares(address, uint256 )",
+        ]);
+        const data = decode.decodeFunctionData("buyShares", txInfo.data);
+        const params = [data[0], 1];
+        sharesList.push(params);
+      }
+      return createBid(sharesList);
     } catch (error) {
-        return start()
+      return start();
     }
-}
+  };
 
-const start = async () => (await fetch())
+  const createBid = async function (params) {
+    try {
+      const contract = new ethers.Contract(
+        "0x87da6930626Fe0c7dB8bc15587ec0e410937e5DC",
+        ["function buyShares(address, uint256)"],
+        wallet
+      );
 
-start()
+      for (const z of params) {
+        const data = contract.interface.encodeFunctionData("buyShares", [
+          z[0],
+          z[1],
+        ]);
+
+        return placeBid(data);
+      }
+    } catch (error) {
+      console.log("\nErro em createBid.");
+      return start();
+    }
+  };
+
+  const placeBid = async function (raw) {
+    try {
+      let data = {
+        rawData: raw,
+        value: 0.000071875,
+        to: "0x87da6930626Fe0c7dB8bc15587ec0e410937e5DC",
+      };
+
+      let config = {
+        method: "post",
+        maxBodyLength: Infinity,
+        url: "https://api.post.tech/wallet-post/wallet/send-transaction",
+        headers: {
+          Authorization:
+            "eyJhbGciOiJSUzI1NiIsImtpZCI6ImFkNWM1ZTlmNTdjOWI2NDYzYzg1ODQ1YTA4OTlhOWQ0MTI5MmM4YzMiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiTWF0aGV1cyBNLiAoU2lubmVycz8pIPCflbjvuI8iLCJwaWN0dXJlIjoiaHR0cHM6Ly9wYnMudHdpbWcuY29tL3Byb2ZpbGVfaW1hZ2VzLzE2NDI5MzQ3MTYwNzk2NjkyNDkvTUpQejJkX3dfbm9ybWFsLmpwZyIsImlzcyI6Imh0dHBzOi8vc2VjdXJldG9rZW4uZ29vZ2xlLmNvbS9wb3N0LXRlY2gtcHJvZCIsImF1ZCI6InBvc3QtdGVjaC1wcm9kIiwiYXV0aF90aW1lIjoxNjk1OTkxMTU3LCJ1c2VyX2lkIjoiVEx1TFZxV1gyWGUxbWNraWxrRkJuYTZXb1pUMiIsInN1YiI6IlRMdUxWcVdYMlhlMW1ja2lsa0ZCbmE2V29aVDIiLCJpYXQiOjE2OTYwMDc2NjIsImV4cCI6MTY5NjAxMTI2MiwiZW1haWwiOiJncy5tYXRoLm1tQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJ0d2l0dGVyLmNvbSI6WyIzNDM4NTg3MzI3Il0sImVtYWlsIjpbImdzLm1hdGgubW1AZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoidHdpdHRlci5jb20ifX0.IoThoEpJgV82wcIQMdItqrsEkiWi5RMWNP_HRUex-EJGjkQZRIqOdZcJr6FuB1nMozv5FN--2oMw5Q_b4LV4plg8XJo8RvNCxreSP1dpYN-WSO2vu41LvisO2wdM7gfrz9Lgv9xJvY_SYOQA_HFwRz86TKRaIHLajQ3-skb0UL1XhIMxnuXi49VNcCVhcanpQfftartS1o2eJztAUH2onez-pDT9zGlbaB4pn-VUWTTHUclBt082R3LVeu2OuvqmV8Ch8V2ci9jztqxeMyTDuijvoV35IMiCe4U3fkCosPxeYGLM7hiR4ldzCb_oyOFdjss8SDQR8lJ65kVvFt2Cfg",
+          "Content-Type": " application/json",
+        },
+        data: data,
+      };
+
+      const response = await axios.request(config);
+      const result = JSON.stringify(response.data.data.tx_hash);
+      console.log(`\n${result}`);
+      return start();
+    } catch (error) {
+      if (error.response.status == 400) {
+        console.log("\nFalhou!");
+      } else if (error.response.status == 401) {
+        console.log(error.response.data.message);
+      }
+
+      return start();
+      // console.log(error.response.data.message);
+    }
+  };
+
+  await fetch();
+};
+
+start();
--- src/sell.js
+++ src/sell.js
@@ -1,95 +1,139 @@
-const axios = require('axios')
-const ethers = require('ethers')
+const axios = require("axios");
+const ethers = require("ethers");
 const { Alchemy, Network, fromHex } = require("alchemy-sdk");
-require('dotenv').config()
+require("dotenv").config();
 
 const config = {
-    apiKey: "ptdQezDinbQE_dIzDAKA0XOaFlYnNo2x",
-    network: Network.ARB_MAINNET,
-  };
+  apiKey: "ptdQezDinbQE_dIzDAKA0XOaFlYnNo2x",
+  network: Network.ARB_MAINNET,
+};
 const alchemy = new Alchemy(config);
-const provider = new ethers.WebSocketProvider(process.env.provider)
-const wallet = new ethers.Wallet(process.env.privatekey,provider)
-const fromAddress = "0x1310cbf77ec2f8c7745739e64dc2df8860bcf07e"
-
+const provider = new ethers.WebSocketProvider(process.env.provider);
+const wallet = new ethers.Wallet(process.env.privatekey, provider);
+const fromAddress = "0x1310cbf77ec2f8c7745739e64dc2df8860bcf07e";
 
 // Main function to fetch all transactions for an address
 const main = async () => {
-    try {
-        // Get all transactions for an address from block 0 and store in txns
-        const txns = await alchemy.core.getAssetTransfers({
-        fromBlock: "0x0",
-        fromAddress: fromAddress,
-        category: ["external"],
-        });
+  try {
+    // Get all transactions for an address from block 0 and store in txns
+    const txns = await alchemy.core.getAssetTransfers({
+      fromBlock: "0x0",
+      excludeZeroValue: true,
+      maxCount: 50,
+      fromAddress: fromAddress,
+      category: ["external"],
+    });
 
-        const {transfers} = txns
-        const hashList = []
-        for(const x of transfers){
-            const {value} = x
-            if(value == 0.000071875){
-                const {hash} = x
-                hashList.push(hash)
-            }
-        }
-        return queryShares(hashList)
-    } catch (error) {
-        console.error('Error fetching transaction history:', error);
+    const { transfers } = txns;
+    const hashList = [];
+    for (const x of transfers) {
+      const { value } = x;
+      if (value == 0.000071875) {
+        const { hash } = x;
+        hashList.push(hash);
+      }
     }
-}
+    return queryShares(hashList);
+  } catch (error) {
+    console.error("Error fetching transaction history:", error);
+  }
+};
 
-const queryShares = async function(list) {
-    const sharesList= []
-    for(const y of list){
-        if (list.indexOf(y) < 20) {
-            const txInfo = await provider.getTransaction(y)
-            const decode = new ethers.Interface(["function buyShares(address, uint256 )"])
-            const data = decode.decodeFunctionData('buyShares', txInfo.data)
-            const params = [data[0], 1]
-            sharesList.push(params)
-        }
+const queryShares = async function (list) {
+  try {
+    const sharesList = [];
+    for (const y of list) {
+      if (list.indexOf(y) < 10) {
+        const txInfo = await provider.getTransaction(y);
+        const decode = new ethers.Interface([
+          "function buyShares(address, uint256 )",
+        ]);
+        const data = decode.decodeFunctionData("buyShares", txInfo.data);
+        const params = [data[0], 1];
+        sharesList.push(params);
+      }
     }
-    return createSell(sharesList)
-}
+    return createSell(sharesList);
+  } catch (error) {
+    return start();
+  }
+};
 
+const placeSell = async function (raw) {
+  try {
+    let data = {
+      rawData: raw,
+      value: 0,
+      to: "0x87da6930626Fe0c7dB8bc15587ec0e410937e5DC",
+      price: 0.00025,
+    };
+
+    let config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: "https://api.post.tech/wallet-post/wallet/send-transaction",
+      headers: {
+        Authorization:
+          "eyJhbGciOiJSUzI1NiIsImtpZCI6ImFkNWM1ZTlmNTdjOWI2NDYzYzg1ODQ1YTA4OTlhOWQ0MTI5MmM4YzMiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiTWF0aGV1cyBNLiAoU2lubmVycz8pIPCflbjvuI8iLCJwaWN0dXJlIjoiaHR0cHM6Ly9wYnMudHdpbWcuY29tL3Byb2ZpbGVfaW1hZ2VzLzE2NDI5MzQ3MTYwNzk2NjkyNDkvTUpQejJkX3dfbm9ybWFsLmpwZyIsImlzcyI6Imh0dHBzOi8vc2VjdXJldG9rZW4uZ29vZ2xlLmNvbS9wb3N0LXRlY2gtcHJvZCIsImF1ZCI6InBvc3QtdGVjaC1wcm9kIiwiYXV0aF90aW1lIjoxNjk1OTkxMTU3LCJ1c2VyX2lkIjoiVEx1TFZxV1gyWGUxbWNraWxrRkJuYTZXb1pUMiIsInN1YiI6IlRMdUxWcVdYMlhlMW1ja2lsa0ZCbmE2V29aVDIiLCJpYXQiOjE2OTYwMTc1NjUsImV4cCI6MTY5NjAyMTE2NSwiZW1haWwiOiJncy5tYXRoLm1tQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJ0d2l0dGVyLmNvbSI6WyIzNDM4NTg3MzI3Il0sImVtYWlsIjpbImdzLm1hdGgubW1AZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoidHdpdHRlci5jb20ifX0.chxTkY2Fmm9h8hKH4spbo9xr5ihUoIKYGWkIe6qwY-CNuCprmZTVZ6uVKsF8kzOEQAKFoElgJKrmiMLVOEgEr2-bxIHMLpbEamJFG3buWflekRjNM4b_DS2Xrh-7vR_Od2_RzZz60DayuUAVWLMPEjDWKMfeurQ-pUYGY3uv0QWWhHIcgVbQnbdwX8huol0SDvmgFVJc9FYZh8tfOzG0TbBERmDZEcJBg8mj8BEOwL6Ir1--nIUBRwanox0uy2BTO5C05yaVEc--OR5cssUTloDAt7SMuVAXvzYtYHXro1ydwM9XY_X8QNHZJDM_N2oO0faRkbbkYam6LDWXxG5G8A",
+        "Content-Type": " application/json",
+      },
+      data: data,
+    };
+
+    const response = await axios.request(config);
+    const result = JSON.stringify(response.data.data.tx_hash);
+    console.log(`\n${result}`);
+  } catch (error) {
+    if (error.response.status == 400) {
+      console.log("\nFalhou!");
+      return await main();
+    } else if (error.response.status == 401) {
+      console.log(error.response.data.message);
+    }
+  }
+};
 
 const createSell = async function (params) {
-    try {
-        const contractGetPrice = new ethers.Contract('0x87da6930626Fe0c7dB8bc15587ec0e410937e5DC',["function getSellPrice(address, uint256)"],wallet)
-  
-        for (const z of params) {
+  try {
+    const abi =
+      '[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"trader","type":"address"},{"indexed":false,"internalType":"address","name":"subject","type":"address"},{"indexed":false,"internalType":"bool","name":"isBuy","type":"bool"},{"indexed":false,"internalType":"uint256","name":"shareAmount","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"ethAmount","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"protocolEthAmount","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"subjectEthAmount","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"holderEthAmount","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"referralEthAmount","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"supply","type":"uint256"}],"name":"Trade","type":"event"},{"inputs":[{"internalType":"address","name":"sharesSubject","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"buyShares","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"address","name":"sharesSubject","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"getBuyPrice","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"sharesSubject","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"getBuyPriceAfterFee","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"supply","type":"uint256"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"getPrice","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"address","name":"sharesSubject","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"getSellPrice","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"sharesSubject","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"getSellPriceAfterFee","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"holderAndReferralFeeDestination","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"holderFeePercent","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"protocolFeeDestination","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"protocolFeePercent","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"referralFeePercent","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"sharesSubject","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"sellShares","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"address","name":"_feeDestination","type":"address"}],"name":"setFeeDestination","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_feeDestination","type":"address"}],"name":"setHolderFeeDestination","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_feePercent","type":"uint256"}],"name":"setProtocolFeePercent","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_feePercent","type":"uint256"}],"name":"setSubjectFeePercent","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_feePercent","type":"uint256"}],"name":"sethHolderFeePercent","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_feePercent","type":"uint256"}],"name":"sethReferralFeePercent","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"}],"name":"sharesBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"sharesSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"subjectFeePercent","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"}]';
 
-            const info = await contractGetPrice.getSellPrice(z[0],z[1])
-            if(info == 250000000000000){
-                console.log('\nTem um no preço.')
-                const contract = new ethers.Contract('0x87da6930626Fe0c7dB8bc15587ec0e410937e5DC',["function sellShares(address, uint256 )"],wallet)
+    const contract = new ethers.Contract(
+      "0x87da6930626Fe0c7dB8bc15587ec0e410937e5DC",
+      abi,
+      wallet
+    );
 
-                for (const a of params) {
-        
-                    const tx = await contract.sellShares(a[0],a[1],{
-                        gasLimit: 300000
-                    })
-                    const txhash = await tx.wait()
-                    console.log(txhash.hash)
-                }
-            } else {
-                continue
-            }
+    for (const z of params) {
+      const info = await contract.getSellPrice(z[0], z[1]);
+      const numberInfo = parseInt(info.toString());
+      if (numberInfo == 250000000000000) {
+        console.log("\nTem um no preço.");
+
+        for (const a of params) {
+          const data = contract.interface.encodeFunctionData("sellShares", [
+            a[0],
+            a[1],
+          ]);
+          await placeSell(data);
         }
-    } catch (error) {
-        return main()
+      } else {
+        continue;
+      }
     }
-}
+  } catch (error) {
+    return main();
+  }
+};
 
-const start = async function(){
-    while(true){
-        console.log('\nIniciando.')
-        await main();
-        console.log('\nAguardando.')
-        await new Promise (resolve => setTimeout(resolve, 1000*60*5))
-        console.log('\nPróximo loop.')
-    }
-}
+const start = async function () {
+  while (true) {
+    console.log("\nIniciando.");
+    await main();
+    console.log("\nAguardando.");
+    await new Promise((resolve) => setTimeout(resolve, 1000 * 60 * 5));
+    console.log("\nPróximo loop.");
+  }
+};
 
-
-start()
+start();
